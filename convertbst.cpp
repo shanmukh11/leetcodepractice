552a5988e5cfc1a1012e8f86a172d9d05cd73c86
shanmukh11 <shanmukhs99@gmail.com>
2018-07-09 19:27:39 +0530

    Convert BST to greater tree

---
+++ convertbst.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+private:
+     int sum=0;
+public:
+    
+    TreeNode* convertBST(TreeNode* root) {
+       
+        if(!root) return root;
+        
+        root->right=convertBST(root->right);
+        root->val+=sum;
+        sum=root->val;
+        root->left=convertBST(root->left);
+        
+        return root;
+    }
+};
