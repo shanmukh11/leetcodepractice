d0e35cadc1fa857c7fa6b3df2fb761b916b53170
shanmukh11 <shanmukhs99@gmail.com>
2018-07-09 19:15:47 +0530

    SubTree Check

--- subtree.cpp
+++ subtree.cpp
@@ -1,12 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+
 class Solution {
 public:
     bool comptree(TreeNode* s,TreeNode* t)
