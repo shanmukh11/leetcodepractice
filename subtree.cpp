2e1b18a7b0d0dfcc858c95a3a3dd95ca4b2723ff
shanmukh11 <shanmukhs99@gmail.com>
2018-07-09 19:15:30 +0530

    Minimum Path Sum

---
+++ subtree.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool comptree(TreeNode* s,TreeNode* t)
+    {
+        if(s==NULL&&t==NULL)
+            return true;
+        if(s==NULL||t==NULL)
+            return false;
+        
+        return (s->val==t->val)&&comptree(s->right,t->right)&&comptree(s->left,t->left);
+    }
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+       
+        if(s!=NULL &&(comptree(s,t)|| isSubtree(s->left,t)||isSubtree(s->right,t)))
+            return true;
+        else
+        return false;
+    }
+};
